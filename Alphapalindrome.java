aa2a3f2d5034a19742c1af07bedc879c23c2a349
srisanthini <srisanthinisivanandham@gmail.com>
2026-01-06 12:22:05 +0530

    Add files via upload

---
+++ Alphapalindrome.java
@@ -0,0 +1,24 @@
+
+public class Alphapalindrome {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<n;j++){
+                System.out.print("  ");
+            }
+            char ch=(char)('A'+i-1);
+            for(int k=1;k<=i;k++){
+                System.out.print(ch+" ");
+                ch--;
+            }
+            ch+=2;
+            for(int l=2;l<=i;l++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
